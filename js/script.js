97f72460d6bae514fb426c56d5a953358a10583d
keymaru-500ml <chiaki.nwebcoder@gmail.com>
2025-09-15 16:42:32 +0900

    Update script.js
    ハンバーガーメニューのエラーの修正

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(function() {
   const headerHeight = 88; // ヘッダーの高さを設定
+ const pcWidth = 1021;    // PCは1021px以上と判定
 
   // ハンバーガーボタンの開閉
   $("#js-button-drawer").on("click", function(){
@@ -10,19 +11,21 @@ $(function() {
 
   // メニュー内リンクをクリックしたとき
   $("#js-drawer .header__nav-link").on("click", function(e){
-      // メニューを閉じる
-      $("#js-button-drawer").removeClass("is-checked");
-      $("#js-drawer").slideUp();
-      $("body").removeClass("is-fixed");
-
       const target = $(this).attr("href");
 
+      // 画面幅がPC未満のときだけメニューを閉じる
+      if ($(window).width() < pcWidth) {
+          $("#js-button-drawer").removeClass("is-checked");
+          $("#js-drawer").slideUp();
+          $("body").removeClass("is-fixed");
+      }
+
       // ページ内リンクの場合はスムーズスクロール
-      if(target.startsWith("#")) {
+      if (target.startsWith("#")) {
           e.preventDefault(); // デフォルト遷移を防ぐ
           const top = $(target).offset().top - headerHeight; // ヘッダー分を引く
           $("html, body").animate({ scrollTop: top }, 500);
       }
-      // 外部リンクはそのまま遷移
   });
 });
+
